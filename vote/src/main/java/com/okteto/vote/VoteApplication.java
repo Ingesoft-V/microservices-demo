c650ebc5b3a16e900bdd4c9fa91ee897708923fa
David104087 <1040871155@u.icesi.edu.co>
2026-04-12 19:54:50 -0500

    fix(VoteApplication): add comment to clarify main entry point of the application

--- vote/src/main/java/com/okteto/vote/VoteApplication.java
+++ vote/src/main/java/com/okteto/vote/VoteApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VoteApplication {
 
+	// This is the main entry point of the Spring Boot application. It starts the application context and the embedded server.
 	public static void main(String[] args) {
 		SpringApplication.run(VoteApplication.class, args);
 	}
